16e04a31e140e81070b30424d52ca9a99d406244
zip184 <zip184@gmail.com>
2021-03-24 21:04:52 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,94 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "walkRight":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f f . . . . 
+. . . . f f e e e e f 2 f . . . 
+. . . f f e e e e f 2 2 2 f . . 
+. . . f e e e f f e e e e f . . 
+. . . f f f f e e 2 2 2 2 e f . 
+. . . f e 2 2 2 f f f f e 2 f . 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e e e d d d f . . . 
+. . . . . f 4 d d e 4 e f . . . 
+. . . . . f e d d e 2 2 f . . . 
+. . . . f f f e e f 5 5 f f . . 
+. . . . f f f f f f f f f f . . 
+. . . . . f f . . . f f f . . . 
+`;
+            case "image3":
+            case "walkLeft":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f 2 f e e e e f f . . . . 
+. . f 2 2 2 f e e e e f f . . . 
+. . f e e e e f f e e e f . . . 
+. f e 2 2 2 2 e e f f f f . . . 
+. f 2 e f f f f 2 2 2 e f . . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d e e e e e f . . . 
+. . . f e 4 e d d 4 f . . . . . 
+. . . f 2 2 e d d e f . . . . . 
+. . f f 5 5 f e e f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . . f f f . . . f f . . . . . 
+`;
+            case "image1":
+            case "walkRightFlipped":return img`
+. . . . . f f . . . f f f . . . 
+. . . . f f f f f f f f f f . . 
+. . . . f f f e e f 5 5 f f . . 
+. . . . . f e d d e 2 2 f . . . 
+. . . . . f 4 d d e 4 e f . . . 
+. . . f e e e e e d d d f . . . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f f f f f f f e e e f f f . 
+. . . f e 2 2 2 f f f f e 2 f . 
+. . . f f f f e e 2 2 2 2 e f . 
+. . . f e e e f f e e e e f . . 
+. . . f f e e e e f 2 2 2 f . . 
+. . . . f f e e e e f 2 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "walkLeftFlipped":return img`
+. . . f f f . . . f f . . . . . 
+. . f f f f f f f f f f . . . . 
+. . f f 5 5 f e e f f f . . . . 
+. . . f 2 2 e d d e f . . . . . 
+. . . f e 4 e d d 4 f . . . . . 
+. . . f d d d e e e e e f . . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. f f f e e e f f f f f f f . . 
+. f 2 e f f f f 2 2 2 e f . . . 
+. f e 2 2 2 2 e e f f f f . . . 
+. . f e e e e f f e e e f . . . 
+. . f 2 2 2 f e e e e f f . . . 
+. . . f 2 f e e e e f f . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,80 @@
+namespace SpriteKind {
+    export const Switch = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    if (isFlipped) {
+        scrola.ay = GRAVITY
+        isFlipped = 0
+    } else {
+        scrola.ay = FLIPPED_GRAVITY
+        isFlipped = 1
+        tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`transparency16`)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (scrola.vy == 0) {
+        if (isFlipped) {
+            scrola.vy = JUMPED_FLIPPED
+        } else {
+            scrola.vy = JUMP
+        }
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (isFlipped) {
+        scrola.setImage(assets.image`walkLeftFlipped`)
+    } else {
+        scrola.setImage(assets.image`walkLeft`)
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (isFlipped) {
+        scrola.setImage(assets.image`walkRightFlipped`)
+    } else {
+        scrola.setImage(assets.image`walkRight`)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fireBottom`, function (sprite, location) {
+    game.over(false, effects.melt)
+})
+sprites.onOverlap(SpriteKind.Switch, SpriteKind.Player, function (sprite, otherSprite) {
+    if (isFlipped) {
+        scrola.ay = GRAVITY
+        isFlipped = 0
+    } else {
+        scrola.ay = FLIPPED_GRAVITY
+        isFlipped = 1
+    }
+    sprite.destroy()
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fireTop`, function (sprite, location) {
+    game.over(false, effects.melt)
+})
+let isFlipped = 0
+let scrola: Sprite = null
+let JUMPED_FLIPPED = 0
+let JUMP = 0
+let FLIPPED_GRAVITY = 0
+let GRAVITY = 0
+GRAVITY = 300
+FLIPPED_GRAVITY = -300
+JUMP = -150
+JUMPED_FLIPPED = 150
+scene.setBackgroundColor(14)
+scrola = sprites.create(assets.image`walkRight`, SpriteKind.Player)
+scrola.ay = GRAVITY
+scene.cameraFollowSprite(scrola)
+tiles.setTilemap(tilemap`level2`)
+controller.moveSprite(scrola, 100, 0)
+tiles.placeOnTile(scrola, tiles.getTileLocation(1, 10))
+let switch1 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+tiles.placeOnTile(switch1, tiles.getTileLocation(56, 11))
+let switch2 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+tiles.placeOnTile(switch2, tiles.getTileLocation(50, 8))
+let switch3 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+tiles.placeOnTile(switch3, tiles.getTileLocation(72, 8))
+let switch4 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+tiles.placeOnTile(switch4, tiles.getTileLocation(25, 7))
