b6121448247ebca1a0261ed43b7ce8f860fa27d1
zip184 <zip184@gmail.com>
2021-03-25 08:53:20 -0400

    Re-organized code into functions

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 namespace SpriteKind {
     export const Switch = SpriteKind.create()
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    if (isFlipped) {
-        scrola.ay = GRAVITY
-        isFlipped = 0
-    } else {
-        scrola.ay = FLIPPED_GRAVITY
-        isFlipped = 1
-    }
-})
+function placeSwitches () {
+    switch1 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+    tiles.placeOnTile(switch1, tiles.getTileLocation(56, 11))
+    switch2 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+    tiles.placeOnTile(switch2, tiles.getTileLocation(50, 8))
+    switch3 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+    tiles.placeOnTile(switch3, tiles.getTileLocation(72, 8))
+    switch4 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
+    tiles.placeOnTile(switch4, tiles.getTileLocation(25, 7))
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (scrola.vy == 0) {
         if (isFlipped) {
@@ -26,6 +27,9 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         scrola.setImage(assets.image`walkLeft`)
     }
 })
+/**
+ * Setup Controllers
+ */
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (isFlipped) {
         scrola.setImage(assets.image`walkRightFlipped`)
@@ -33,9 +37,19 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         scrola.setImage(assets.image`walkRight`)
     }
 })
+/**
+ * Overlap Events
+ */
 scene.onOverlapTile(SpriteKind.Player, assets.tile`fireBottom`, function (sprite, location) {
     game.over(false, effects.melt)
 })
+function setupPlayer () {
+    scrola = sprites.create(assets.image`walkRight`, SpriteKind.Player)
+    tiles.placeOnTile(scrola, tiles.getTileLocation(1, 10))
+    controller.moveSprite(scrola, 100, 0)
+    scrola.ay = GRAVITY
+    scene.cameraFollowSprite(scrola)
+}
 sprites.onOverlap(SpriteKind.Switch, SpriteKind.Player, function (sprite, otherSprite) {
     if (isFlipped) {
         scrola.ay = GRAVITY
@@ -47,34 +61,31 @@ sprites.onOverlap(SpriteKind.Switch, SpriteKind.Player, function (sprite, otherS
     sprite.destroy(effects.fire, 300)
     music.magicWand.play()
 })
+function setConstants () {
+    GRAVITY = 300
+    FLIPPED_GRAVITY = -300
+    JUMP = -150
+    JUMPED_FLIPPED = 150
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
     game.over(true, effects.confetti)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`fireTop`, function (sprite, location) {
     game.over(false, effects.melt)
 })
-let isFlipped = 0
-let scrola: Sprite = null
-let JUMPED_FLIPPED = 0
-let JUMP = 0
 let FLIPPED_GRAVITY = 0
 let GRAVITY = 0
-GRAVITY = 300
-FLIPPED_GRAVITY = -300
-JUMP = -150
-JUMPED_FLIPPED = 150
+let JUMP = 0
+let JUMPED_FLIPPED = 0
+let isFlipped = 0
+let scrola: Sprite = null
+let switch4: Sprite = null
+let switch3: Sprite = null
+let switch2: Sprite = null
+let switch1: Sprite = null
+setConstants()
 scene.setBackgroundColor(14)
-scrola = sprites.create(assets.image`walkRight`, SpriteKind.Player)
-scrola.ay = GRAVITY
-scene.cameraFollowSprite(scrola)
 tiles.setTilemap(tilemap`level2`)
-controller.moveSprite(scrola, 100, 0)
-tiles.placeOnTile(scrola, tiles.getTileLocation(1, 10))
-let switch1 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
-tiles.placeOnTile(switch1, tiles.getTileLocation(56, 11))
-let switch2 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
-tiles.placeOnTile(switch2, tiles.getTileLocation(50, 8))
-let switch3 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
-tiles.placeOnTile(switch3, tiles.getTileLocation(72, 8))
-let switch4 = sprites.create(assets.tile`myTile`, SpriteKind.Switch)
-tiles.placeOnTile(switch4, tiles.getTileLocation(25, 7))
+placeSwitches()
+setupPlayer()
+game.splash("Anti-Gravity Scroller")
